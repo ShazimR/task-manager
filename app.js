dfaec7fa975212b4588b623f05ea6987cddee699
Shazim Rahman <shazimrahman@hotmail.com>
2023-07-01 17:16:43 -0400

    added 404 not found page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
 
 
 // middleware
@@ -14,6 +15,7 @@ app.use(express.static('./public'));
 
 // routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
 
 const start = async () => {
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,7 +4,7 @@ const Task = require('../models/Task');
 const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find({});
-        res.status(200).json({ tasks:tasks });
+        res.status(200).json({ tasks });
     } catch (error) {
         res.status(500).json({ msg:error });
     }
